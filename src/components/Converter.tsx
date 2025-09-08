6bcd331a52fcf6a54619fe37f05b995939f84003
Benas Nekrošius <benasnek@gmail.com>
2025-09-08 18:38:35 +0300

    Initial astro commit

---
+++ src/components/Converter.tsx
@@ -0,0 +1,8 @@
+import FileUpload from "./FileUpload.tsx";
+
+export default function Converter() {
+    return <div>
+        <FileUpload />
+        testss
+    </div>
+}
---
+++ src/components/FileUpload.tsx
@@ -0,0 +1,27 @@
+import {useCallback} from "react";
+import {useDropzone} from "react-dropzone";
+
+export default function FileUpload() {
+    // const onDrop = useCallback((acceptedFiles: File[]) => {
+    //     console.log(acceptedFiles);
+    // }, []);
+
+    const {acceptedFiles, getRootProps, getInputProps} = useDropzone();
+
+    const files = acceptedFiles.map(file => (
+        <li key={file.path}>
+            {file.path} - {file.size} bytes
+        </li>
+    ));
+
+    return <section className="container">
+        <div {...getRootProps({className: 'dropzone'})}>
+            <input {...getInputProps()} />
+            <p>Drag 'n' drop some files here, or click to select files</p>
+        </div>
+        <aside>
+            <h4>Files</h4>
+            <ul>{files}</ul>
+        </aside>
+    </section>
+}
